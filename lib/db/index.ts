0b9ee47bf65fbf0f71e543cce39ec84e795b7744
Jean Ruiz <jeancarlosruizv@gmail.com>
2025-12-22 15:29:39 -0400

    updating index to fix building error

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,12 +2,8 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL environment variable is not set");
-}
-
 // Create postgres connection
-const client = postgres(process.env.DATABASE_URL, {
+const client = postgres(process.env.DATABASE_URL!, {
   prepare: false,
 });
 
