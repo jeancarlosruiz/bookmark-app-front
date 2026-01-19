f9a9dbbb0e7826b5e61841f6567ad7f3def447a1
Jean Ruiz <jeanruiz@lugotech.com>
2026-01-19 16:16:13 -0400

    cleaning up files used in development proccess

--- app/examples/confirmation-dialogs/page.tsx
+++
@@ -1,5 +0,0 @@
-import { BookmarkConfirmationDialogsExample } from "@/components/organisms/bookmark-confirmation-dialogs.example";
-
-export default function ConfirmationDialogsExamplePage() {
-  return <BookmarkConfirmationDialogsExample />;
-}
